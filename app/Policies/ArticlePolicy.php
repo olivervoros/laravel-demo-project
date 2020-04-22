6347b3e2fd529c1a45bcd00c882058e1f66f16fa
Oliver Voros <olivervoros@gmail.com>
2020-04-22 17:57:59 +0200

    custom responses

--- app/Policies/ArticlePolicy.php
+++ app/Policies/ArticlePolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 use App\Article;
 use App\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 class ArticlePolicy
 {
@@ -30,7 +31,9 @@ class ArticlePolicy
      */
     public function view(User $user, Article $article)
     {
-        return $user->id === $article->user_id;
+        return $user->id === $article->user_id
+            ? Response::allow()
+            : Response::deny('You are only allowed to view your own articles... - (see ArticlePolicy)');
     }
 
     /**
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Article;
 use App\Policies\ArticlePolicy;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -28,11 +29,15 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::define('view-admin-dashboard', function ($user) {
-            return $user->isAdmin();
+            return $user->isAdmin()
+                ? Response::allow()
+                : Response::deny('You must be an Administrator to view the dashboard, and your role is: '.$user->role->role);
         });
 
         Gate::define('view-article', function ($user, $article) {
-            return $user->id === $article->user_id;
+            return ($user->id === $article->user_id)
+                ? Response::allow()
+                : Response::deny('You are only allowed to view your own articles... - (see AuthServiceProvider Gate)');
         });
     }
 }
